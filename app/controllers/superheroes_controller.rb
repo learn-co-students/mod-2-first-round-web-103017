246b3caedcce483838bd5f71f332b22e4609b64e
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-12-04 11:00:49 -0500

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,31 @@
 class SuperheroesController < ApplicationController
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.create(hero_params)
+    @id = @superhero.id
+    redirect_to superhero_path(@id)
+  end
+
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+
+
+  private
+
+  def hero_params
+    params.require(:superhero).permit(:name, :description, :superpower_id)
+  end
+
+
+
 end
