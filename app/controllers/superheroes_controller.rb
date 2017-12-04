5d72c82ece1c3ea4311f9f7618e5ba99c476f48d
c0nniewang <connie.wang303@gmail.com>
2017-12-04 11:00:33 -0500

    Done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,36 @@
 class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
+
+    if params[:search]
+      @superheroes = Superhero.search(params[:search])
+    else
+      @superheroes = Superhero.all
+    end
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+
+    if @superhero.valid?
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,8 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates :super_name, uniqueness: true
+
+  def self.search(search)
+    Superhero.joins(:superpower).where('superpowers.name = ?', "#{search}")
+  end
 end
