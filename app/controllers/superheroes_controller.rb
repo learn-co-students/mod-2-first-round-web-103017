d8ac631491d3371c681e60f5a8cf5118a9232148
pawlkris <paulkristapovich@gmail.com>
2017-12-04 11:01:20 -0500

    submission

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,28 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+    @superpower = @superhero.superpower
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+    redirect_to superhero_path(@superhero)
+  end
+  # 
+  # def search
+  #   @projects = Project.search(params[:search])
+  # end
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:super_name, :name, :superpower_id)
+  end
+
 end
