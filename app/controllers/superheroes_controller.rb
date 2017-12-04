5bde0a027f84f189eb2dc679ed3a0b16389fc3bb
Josh-Stillman <joshstillman@gmail.com>
2017-12-04 10:52:49 -0500

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,34 @@
 class SuperheroesController < ApplicationController
+
   def index
-    @superheroes = Superhero.all
+    if params[:q].nil? || Superpower.where(name: params[:q]).empty?
+      @superheroes = Superhero.all
+    else
+      sp_id = Superpower.where(name: params[:q]).first.id
+      @superheroes = Superhero.where(superpower_id: sp_id)
+    end
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    if @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
   end
 end
---
+++ db/migrate/20171204143609_add_powers_to_super_heroes.rb
@@ -0,0 +1,5 @@
+class AddPowersToSuperHeroes < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :superheroes, :superpower, foreign_key: true
+  end
+end
