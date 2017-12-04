afa803cb93d900c7e5e8d8fe63cbc98b1853fa7f
chris <christofer.d.johnson@gmail.com>
2017-12-04 10:50:52 -0500

    Done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,44 @@
 class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
+
   end
+
+  def new
+  	@superhero = Superhero.new
+  end 
+
+  def search
+    @superpowers = Superpower.find_by(name: params[:query])
+
+  end
+
+
+  def show
+  	@superhero = Superhero.find(params[:id])
+  	@superpower = Superpower.find(@superhero.superpower_id)
+  end 
+
+  def create
+  	#byebug
+
+  	@superhero = Superhero.new(super_params)
+
+  	if @superhero.valid?
+  		@superhero.save
+  		redirect_to superhero_path(@superhero)
+  	else
+  		render :new
+  	end
+
+
+  end
+
+  private
+
+  def super_params
+  	params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
+
 end
