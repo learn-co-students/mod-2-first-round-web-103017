d2a5f0692b54d660381108b6c45556177c8632a1
psarma89 <priyam.sarma@gmail.com>
2017-12-04 11:00:27 -0500

    Adds final version

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,34 @@
 class SuperheroesController < ApplicationController
   def index
+    # raise params.inspect
     @superheroes = Superhero.all
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def create
+    # raise params.inspect
+    @superhero = Superhero.create(superhero_params(:name, :super_name, :superpower_id))
+    redirect_to superhero_path(@superhero)
+
+    # @superhero = Superhero.new(superhero_params(:name, :super_name, :superpower_id))
+    # if @superhero.save
+    #   redirect_to superhero_path(@superhero)
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+
+  def superhero_params(*args)
+    params.require(:superhero).permit(*args)
   end
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,15 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+  # def search
+  #   @superpower = Superpower.find_by(name: params[:name])
+  #   @superheroes = @superpower.superheroes
+  #   redirect_to superheroes_path
+  #
+  # end
 end
