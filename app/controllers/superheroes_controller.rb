89021c2a85463ac85f34d593d0f097dd3beec6de
Silvia Chung <silviachung@Silvias-MacBook.local>
2017-12-04 11:01:10 -0500

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,30 @@
 class SuperheroesController < ApplicationController
   def index
-    @superheroes = Superhero.all
+      byebug
+    if params[:superpower]
+      @superheroes = Superhero.where('store LIKE ?', "%#{params[:superpower]}%")
+    else
+      @superheroes = Superhero.all
+    end
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpower = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params(:name, :super_name, :superpower_id))
+    redirect_to superhero_path(@superhero)
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  private
+
+  def superhero_params(*args)
+    params.require(:superhero).permit(*args)
   end
 end
