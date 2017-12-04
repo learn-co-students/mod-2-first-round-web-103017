4cd03be9a3f2ce7c67dad8b4c12c5ba870dde942
erickalexander <erickcamacho1@gmail.com>
2017-12-04 11:01:14 -0500

    mod 2 assessment

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,22 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+
+  end
+
+  private def superhero_params
+    params.require(:superhero).permit(:name, :super_name, superpower_ids:[])
+  end
 end
